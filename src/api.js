ab8129d7a27709f51ccb11d1ae0b353e8a9b777c
Justin Eastin <worldmisanthropy199@gmail.com>
2022-07-20 14:27:26 -0400

    City Search
    built the search bar, and integrated the city API

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6052023050mshaf57178e13624e4p1751d1jsn07a15d801bf9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
