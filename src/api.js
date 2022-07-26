b83a078f9de43e14d0c5c0ded53b80e29a8aaa2b
Justin Eastin <worldmisanthropy199@gmail.com>
2022-07-26 16:23:40 -0400

    Forecast and finish
    added a 7 day future forecast with icons and more details that can be opened by clicking the accordion button. then finish stylings and last test

--- src/api.js
+++ src/api.js
@@ -3,8 +3,11 @@ export const geoApiOptions = {
 	headers: {
 		'X-RapidAPI-Key': '6052023050mshaf57178e13624e4p1751d1jsn07a15d801bf9',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+	},
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d109544c495a0ad5d3d1b55dcf98e6e2";
